166f98f0b2f0a75d597f9fa1036d958b4df89751
Wang <dazhi.wang@phys.ufl.edu>
2019-11-21 18:09:00 -0500

    Change checkpoints folder and pause before start

--- Chapter3/full-gan/run.py
+++ Chapter3/full-gan/run.py
@@ -6,9 +6,12 @@ HEIGHT  = 28
 WIDTH   = 28
 CHANNEL = 1
 LATENT_SPACE_SIZE = 100
-EPOCHS = 50001
+EPOCHS = 10000
 BATCH = 32
-CHECKPOINT = 500
+CHECKPOINT = 1000
+# MODEL_TYLE:
+#   -1: Train on all numbers from 0 to 9
+#    x: Train only for digit x. This will make the convergence faster.
 MODEL_TYPE = -1
 
 trainer = Trainer(height=HEIGHT,\
@@ -19,5 +22,7 @@ trainer = Trainer(height=HEIGHT,\
                  batch=BATCH,\
                  checkpoint=CHECKPOINT,
                  model_type=MODEL_TYPE)
-                 
-trainer.train()
+if input("Enter y to continue training: ") == 'y':
+    trainer.train()
+else:
+    print("Bye")
